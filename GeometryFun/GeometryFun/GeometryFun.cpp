7d82d5d4ae4aae8934eb7605050d5d2220d4d153
Nick McCollum <McCollumN@webtrends.com>
2018-10-04 17:32:20 -0700

    Initial push

---
+++ GeometryFun/GeometryFun/GeometryFun.cpp
@@ -0,0 +1,45 @@
+/*
+@Filename:		GeometryFun.cpp
+@Author:		Nick McCollum
+@Date:			10/4/2018
+@Version:		1.0
+@Dev env:		VS 2017
+@Description:	Prompts a user for a positive whole number and outputs:
+					The perimeter and area of a square whose side length is the entered number
+					The radius, circumference, and area of a circle whose diameter is the entered number
+*/
+
+#include <iostream>
+#include <cmath>		// Includes pow function
+
+using namespace std;
+
+const float PI = 3.14159f;
+
+int main() {
+	int userInput;
+	int perimeter, areaSquare;
+	double radius, circumference, areaCircle;
+
+	// Get input from the user
+	cout << "Please enter a whole number: ";
+	cin >> userInput;
+
+	// Compute values
+	perimeter = userInput * 4;
+	areaSquare = userInput * userInput;
+	radius = userInput / 2.0;
+	circumference = 2 * PI * radius;
+	areaCircle = PI * pow(radius, 2.0);
+
+	// Output values
+	cout << "\nA square with side length of " << userInput << "\n";
+	cout << "\thas a perimeter of " << perimeter << "\n";
+	cout << "\thas an area of " << areaSquare << "\n\n";
+	cout << "A circle with a diameter of " << userInput << "\n";
+	cout << "\thas a radius of " << radius << "\n";
+	cout << "\thas a circumference of " << circumference << "\n";
+	cout << "\thas an area of " << areaCircle << "\n";
+
+	return 1;
+}
