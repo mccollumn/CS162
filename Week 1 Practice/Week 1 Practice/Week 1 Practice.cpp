bf95c484facbd36492e58dede00563deb73beab0
Nick McCollum <nmccollum@frontier.com>
2019-01-10 14:07:36 -0800

    Week 1 Practice

---
+++ Week 1 Practice/Week 1 Practice/Week 1 Practice.cpp
@@ -0,0 +1,15 @@
+
+const int STUDENTS = 24;
+const int DAYS = 20;
+const int ROWS = 9;
+const int COLS = 9;
+
+int main() {
+	// an array for daily attendence for 24 students and 20 class days
+	bool attendance[STUDENTS][DAYS];
+
+	// an array to represent a 9x9 sudoku puzzle
+	char sudoku[ROWS][COLS];
+
+	return 0;
+}
