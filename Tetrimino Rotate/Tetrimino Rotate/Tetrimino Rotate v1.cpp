52dd1be2f901da2a21649fbf3f4a223c10f8af87
Nick McCollum <nmccollum@frontier.com>
2019-01-13 13:34:08 -0800

    Tetrimino Rotate v2

---
+++ Tetrimino Rotate/Tetrimino Rotate/Tetrimino Rotate v1.cpp
@@ -0,0 +1,192 @@
+/*
+@Filename:		Tetrimino Rotate.cpp
+@Author:		Nick McCollum
+@Date:			1/10/2019
+@Version:		1.0
+@Dev env:		VS 2017
+@Description:	Generate seven unique 2-dimensional arrays that represent Tetriminos.
+				Rotates the tetriminos left and right, then prints output.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int NUM_SHAPES = 7;
+const int NUM_ROWS = 4;
+const int NUM_COLS = 4;
+const int NUM_COORDS = 2;
+
+typedef int tetriminoArray[NUM_ROWS][NUM_COLS];
+
+enum shapes { I, J, L, O, S, T, Z };
+
+/*
+initialize: Sets the initial array values (shape) for each tetrimino
+@param: int Tetrimino Array, int Shape of tetrimino
+@return: none
+*/
+void initialize(int tetriminoArray[][NUM_COLS], shapes tetrimino);
+
+/*
+printTetrimino: Prints the contents of a 2-d tetrimino array to the console window
+@param: int Tetrimino Array
+@return: none
+*/
+void printTetrimino(const int tetriminoArray[][NUM_COLS]);
+
+/*
+rotateLeft: Rotate values in grid 90 degrees left
+@param: int Tetrimino Array
+@return: none
+*/
+void rotateLeft(int tetriminoArray[][NUM_COLS]);
+
+/*
+rotateRight: Rotate values in grid 90 degrees right
+@param: int Tetrimino Array
+@return: none
+*/
+void rotateRight(int tetriminoArray[][NUM_COLS]);
+
+/*
+output: Rotates and prints the tetrimino
+@param: int Tetrimino Array, shapes Current Shape, string suffix
+@return: none
+*/
+void output(int tetriminoArray[][NUM_COLS], shapes tetrimino, string suffix);
+
+/*
+copyArray2D: Copies the contents of a 2D int array with known size to another array of the same size
+@params: int Source Array, int Destination Array
+@return: none
+*/
+void copyArray2D(int sourceArray[][NUM_COLS], int destArray[][NUM_COLS]);
+
+int main() {
+
+	shapes tetrimino;
+
+	// Tetrimino arrays
+	tetriminoArray tetriminoI = {};
+	tetriminoArray tetriminoJ = {};
+	tetriminoArray tetriminoL = {};
+	tetriminoArray tetriminoO = {};
+	tetriminoArray tetriminoS = {};
+	tetriminoArray tetriminoT = {};
+	tetriminoArray tetriminoZ = {};
+
+	// Initialize tetriminos
+	initialize(tetriminoI, I);
+	initialize(tetriminoJ, J);
+	initialize(tetriminoL, L);
+	initialize(tetriminoO, O);
+	initialize(tetriminoS, S);
+	initialize(tetriminoT, T);
+	initialize(tetriminoZ, Z);
+
+	// Print tetriminos
+	for (tetrimino = I; tetrimino <= Z; tetrimino = static_cast<shapes>(tetrimino + 1)) {
+		string suffix = "th";
+		switch (tetrimino) {
+		case I:
+			suffix = "st";
+			output(tetriminoI, tetrimino, suffix);
+			break;
+		case J:
+			suffix = "nd";
+			output(tetriminoJ, tetrimino, suffix);
+			break;
+		case L:
+			suffix = "rd";
+			output(tetriminoL, tetrimino, suffix);
+			break;
+		case O:
+			output(tetriminoO, tetrimino, suffix);
+			break;
+		case S:
+			output(tetriminoS, tetrimino, suffix);
+			break;
+		case T:
+			output(tetriminoT, tetrimino, suffix);
+			break;
+		case Z:
+			output(tetriminoZ, tetrimino, suffix);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+// ------------------------------------------------------------
+void initialize(int tetriminoArray[][NUM_COLS], shapes tetrimino) {
+	int shapeCoords[NUM_SHAPES][NUM_ROWS][NUM_COORDS] = {
+		{{1,0},{1,1},{1,2},{1,3}}, // I
+		{{1,1},{2,1},{2,2},{2,3}}, // J
+		{{1,3},{2,1},{2,2},{2,3}}, // L
+		{{1,1},{1,2},{2,1},{2,2}}, // O
+		{{1,2},{1,3},{2,1},{2,2}}, // S
+		{{1,2},{2,1},{2,2},{2,3}}, // T
+		{{1,1},{1,2},{2,2},{2,3}}  // Z
+	};
+	for (int coord = 0; coord < NUM_ROWS; coord++) {
+		tetriminoArray[shapeCoords[tetrimino][coord][0]][shapeCoords[tetrimino][coord][1]] = 1;
+	}
+}
+
+// ------------------------------------------------------------
+void printTetrimino(const int tetriminoArray[][NUM_COLS]) {
+	for (int row = 0; row < NUM_ROWS; row++) {
+		for (int col = 0; col < NUM_COLS; col++) {
+			cout << tetriminoArray[row][col];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+// ------------------------------------------------------------
+void rotateLeft(int tetriminoArray[][NUM_COLS]) {
+	int tmp[NUM_ROWS][NUM_COLS];
+	for (int row = 0; row < NUM_ROWS; row++) {
+		for (int col = 0; col < NUM_COLS; col++) {
+			tmp[(NUM_COLS - 1) - col][row] = tetriminoArray[row][col];
+		}
+	}
+	copyArray2D(tmp, tetriminoArray);
+}
+
+// ------------------------------------------------------------
+void rotateRight(int tetriminoArray[][NUM_COLS]) {
+	int tmp[NUM_ROWS][NUM_COLS];
+	for (int row = 0; row < NUM_ROWS; row++) {
+		for (int col = 0; col < NUM_COLS; col++) {
+			tmp[col][(NUM_ROWS - 1) - row] = tetriminoArray[row][col];
+		}
+	}
+	copyArray2D(tmp, tetriminoArray);
+}
+
+// ------------------------------------------------------------
+void output(int tetriminoArray[][NUM_COLS], shapes tetrimino, string suffix) {
+	cout << "The " << tetrimino + 1 << suffix << " Tetrimino is:\n";
+	printTetrimino(tetriminoArray);
+	rotateRight(tetriminoArray);
+	cout << "The " << tetrimino + 1 << suffix << " Tetrimino rotated 90 degrees to the right is:\n";
+	printTetrimino(tetriminoArray);
+	rotateLeft(tetriminoArray);
+	rotateLeft(tetriminoArray);
+	cout << "The " << tetrimino + 1 << suffix << " Tetrimino rotated 90 degrees to the left is:\n";
+	printTetrimino(tetriminoArray);
+}
+
+// ------------------------------------------------------------
+void copyArray2D(int sourceArray[][NUM_COLS], int destArray[][NUM_COLS]) {
+	for (int row = 0; row < NUM_ROWS; row++) {
+		for (int col = 0; col < NUM_COLS; col++) {
+			destArray[row][col] = sourceArray[row][col];
+		}
+	}
+}
--- Tetrimino Rotate/Tetrimino Rotate/Tetrimino Rotate.cpp
+++ Tetrimino Rotate/Tetrimino Rotate/Tetrimino Rotate.cpp
@@ -2,7 +2,7 @@
 @Filename:		Tetrimino Rotate.cpp
 @Author:		Nick McCollum
 @Date:			1/10/2019
-@Version:		1.0
+@Version:		2.0
 @Dev env:		VS 2017
 @Description:	Generate seven unique 2-dimensional arrays that represent Tetriminos.
 				Rotates the tetriminos left and right, then prints output.
@@ -18,40 +18,42 @@ const int NUM_ROWS = 4;
 const int NUM_COLS = 4;
 const int NUM_COORDS = 2;
 
+enum shapes { I, J, L, O, S, T, Z };
+
 /*
 initialize: Sets the initial array values (shape) for each tetrimino
-@param: int Tetrimino Array, int Shape of tetrimino
-@return: none
-*/
-void initialize(int tetriminoArray[][NUM_COLS], int shape);
-
-/*
-printTetrimino: Prints the contents of a 2-d tetrimino array to the console window
 @param: int Tetrimino Array
 @return: none
 */
-void printTetrimino(const int tetriminoArray[][NUM_COLS]);
-
-/*
-rotateLeft: Rotate values in grid 90 degrees left
-@param: int Tetrimino Array
-@return: none
-*/
-void rotateLeft(int tetriminoArray[][NUM_COLS]);
-
-/*
-rotateRight: Rotate values in grid 90 degrees right
-@param: int Tetrimino Array
-@return: none
-*/
-void rotateRight(int tetriminoArray[][NUM_COLS]);
+void initialize(int tetriminoArray[][NUM_ROWS][NUM_COLS]);
 
 /*
 output: Rotates and prints the tetrimino
-@param: int Tetrimino Array, shapes Current Shape, string suffix
+@param: int Tetrimino Array
 @return: none
 */
-void output(int tetriminoArray[][NUM_COLS], int tetrimino, string suffix);
+void output(int tetriminoArray[][NUM_ROWS][NUM_COLS]);
+
+/*
+printTetrimino: Prints the contents of a 2-d tetrimino array to the console window
+@param: int Tetrimino Array, Shape to Print
+@return: none
+*/
+void printTetrimino(const int tetriminoArray[][NUM_ROWS][NUM_COLS], shapes tetrimino);
+
+/*
+rotateLeft: Rotate values in grid 90 degrees left
+@param: int Tetrimino Array, Shape to Rotate
+@return: none
+*/
+void rotateLeft(int tetriminoArray[][NUM_ROWS][NUM_COLS], shapes tetriminoShape);
+
+/*
+rotateRight: Rotate values in grid 90 degrees right
+@param: int Tetrimino Array, Shape to Rotate
+@return: none
+*/
+void rotateRight(int tetriminoArray[][NUM_ROWS][NUM_COLS], shapes tetriminoShapes);
 
 /*
 copyArray2D: Copies the contents of a 2D int array with known size to another array of the same size
@@ -60,65 +62,29 @@ copyArray2D: Copies the contents of a 2D int array with known size to another ar
 */
 void copyArray2D(int sourceArray[][NUM_COLS], int destArray[][NUM_COLS]);
 
+/*
+tetriminoName: Returns a char represnting the shape at the specified index
+@params: int Tetrimino index
+@return: none
+*/
+char tetriminoName(int tetrimino);
+
 int main() {
 
-	// Tetrimino arrays
-	int tetriminoI[NUM_ROWS][NUM_COLS] = {};
-	int tetriminoJ[NUM_ROWS][NUM_COLS] = {};
-	int tetriminoL[NUM_ROWS][NUM_COLS] = {};
-	int tetriminoO[NUM_ROWS][NUM_COLS] = {};
-	int tetriminoS[NUM_ROWS][NUM_COLS] = {};
-	int tetriminoT[NUM_ROWS][NUM_COLS] = {};
-	int tetriminoZ[NUM_ROWS][NUM_COLS] = {};
-
-	// Shapes
-	enum shapes { I, J, L, O, S, T, Z };
+	int tetriminos[NUM_SHAPES][NUM_ROWS][NUM_COLS] = {};
 
 	// Initialize tetriminos
-	initialize(tetriminoI, I);
-	initialize(tetriminoJ, J);
-	initialize(tetriminoL, L);
-	initialize(tetriminoO, O);
-	initialize(tetriminoS, S);
-	initialize(tetriminoT, T);
-	initialize(tetriminoZ, Z);
-	
-	// Print tetriminos
-	for (int tetrimino = I; tetrimino <= Z; tetrimino++) {
-		string suffix = "th";
-		switch (tetrimino) {
-		case 0:
-			suffix = "st";
-			output(tetriminoI, tetrimino + 1, suffix);
-			break;
-		case 1:
-			suffix = "nd";
-			output(tetriminoJ, tetrimino + 1, suffix);
-			break;
-		case 2:
-			suffix = "rd";
-			output(tetriminoL, tetrimino + 1, suffix);
-			break;
-		case 3:
-			output(tetriminoO, tetrimino + 1, suffix);
-			break;
-		case 4:
-			output(tetriminoS, tetrimino + 1, suffix);
-			break;
-		case 5:
-			output(tetriminoT, tetrimino + 1, suffix);
-			break;
-		case 6:
-			output(tetriminoZ, tetrimino + 1, suffix);
-			break;
-		}
-	}
+	initialize(tetriminos);
+
+	// Generate output
+	output(tetriminos);
 
 	return 0;
 }
 
 // ------------------------------------------------------------
-void initialize(int tetriminoArray[][NUM_COLS], int shape) {
+void initialize(int tetriminoArray[][NUM_ROWS][NUM_COLS]) {
+	shapes tetriminoShape;
 	int shapeCoords[NUM_SHAPES][NUM_ROWS][NUM_COORDS] = {
 		{{1,0},{1,1},{1,2},{1,3}}, // I
 		{{1,1},{2,1},{2,2},{2,3}}, // J
@@ -128,16 +94,38 @@ void initialize(int tetriminoArray[][NUM_COLS], int shape) {
 		{{1,2},{2,1},{2,2},{2,3}}, // T
 		{{1,1},{1,2},{2,2},{2,3}}  // Z
 	};
-	for (int coord = 0; coord < NUM_ROWS; coord++) {
-		tetriminoArray[shapeCoords[shape][coord][0]][shapeCoords[shape][coord][1]] = 1;
+	for (tetriminoShape = I; tetriminoShape <= Z; tetriminoShape = static_cast<shapes>(tetriminoShape + 1)) {
+		for (int coord = 0; coord < NUM_ROWS; coord++) {
+			tetriminoArray[tetriminoShape][shapeCoords[tetriminoShape][coord][0]][shapeCoords[tetriminoShape][coord][1]] = 1;
+		}
 	}
+	
 }
 
 // ------------------------------------------------------------
-void printTetrimino(const int tetriminoArray[][NUM_COLS]) {
+void output(int tetriminoArray[][NUM_ROWS][NUM_COLS]) {
+	shapes tetriminoShape;
+	string suffix[NUM_SHAPES] = { "st", "nd", "rd", "th", "th", "th", "th" };
+	for (tetriminoShape = I; tetriminoShape <= Z; tetriminoShape = static_cast<shapes>(tetriminoShape + 1)) {
+		char currentShape = tetriminoName(tetriminoShape);
+		cout << "The " << tetriminoShape + 1 << suffix[tetriminoShape] << " Tetrimino (" << currentShape << ") is:\n";
+		printTetrimino(tetriminoArray, tetriminoShape);
+		rotateRight(tetriminoArray, tetriminoShape);
+		cout << "The " << tetriminoShape + 1 << suffix[tetriminoShape] << " Tetrimino (" << currentShape << ") rotated 90 degrees to the right is:\n";
+		printTetrimino(tetriminoArray, tetriminoShape);
+		rotateLeft(tetriminoArray, tetriminoShape);
+		rotateLeft(tetriminoArray, tetriminoShape);
+		cout << "The " << tetriminoShape + 1 << suffix[tetriminoShape] << " Tetrimino (" << currentShape << ") rotated 90 degrees to the left is:\n";
+		printTetrimino(tetriminoArray, tetriminoShape);
+	}
+
+}
+
+// ------------------------------------------------------------
+void printTetrimino(const int tetriminoArray[][NUM_ROWS][NUM_COLS], shapes tetrimino) {
 	for (int row = 0; row < NUM_ROWS; row++) {
 		for (int col = 0; col < NUM_COLS; col++) {
-			cout << tetriminoArray[row][col];
+			cout << tetriminoArray[tetrimino][row][col];
 		}
 		cout << endl;
 	}
@@ -145,38 +133,25 @@ void printTetrimino(const int tetriminoArray[][NUM_COLS]) {
 }
 
 // ------------------------------------------------------------
-void rotateLeft(int tetriminoArray[][NUM_COLS]) {
+void rotateLeft(int tetriminoArray[][NUM_ROWS][NUM_COLS], shapes tetriminoShape) {
 	int tmp[NUM_ROWS][NUM_COLS];
 	for (int row = 0; row < NUM_ROWS; row++) {
 		for (int col = 0; col < NUM_COLS; col++) {
-			tmp[(NUM_COLS - 1) - col][row] = tetriminoArray[row][col];
+			tmp[(NUM_COLS - 1) - col][row] = tetriminoArray[tetriminoShape][row][col];
 		}
 	}
-	copyArray2D(tmp, tetriminoArray);
+	copyArray2D(tmp, tetriminoArray[tetriminoShape]);
 }
 
 // ------------------------------------------------------------
-void rotateRight(int tetriminoArray[][NUM_COLS]) {
+void rotateRight(int tetriminoArray[][NUM_ROWS][NUM_COLS], shapes tetriminoShape) {
 	int tmp[NUM_ROWS][NUM_COLS];
 	for (int row = 0; row < NUM_ROWS; row++) {
 		for (int col = 0; col < NUM_COLS; col++) {
-			tmp[col][(NUM_ROWS - 1) - row] = tetriminoArray[row][col];
+			tmp[col][(NUM_ROWS - 1) - row] = tetriminoArray[tetriminoShape][row][col];
 		}
 	}
-	copyArray2D(tmp, tetriminoArray);
-}
-
-// ------------------------------------------------------------
-void output(int tetriminoArray[][NUM_COLS], int tetrimino, string suffix) {
-	cout << "The " << tetrimino << suffix << " Tetrimino is:\n";
-	printTetrimino(tetriminoArray);
-	rotateRight(tetriminoArray);
-	cout << "The " << tetrimino << suffix << " Tetrimino rotated 90 degrees to the right is:\n";
-	printTetrimino(tetriminoArray);
-	rotateLeft(tetriminoArray);
-	rotateLeft(tetriminoArray);
-	cout << "The " << tetrimino << suffix << " Tetrimino rotated 90 degrees to the left is:\n";
-	printTetrimino(tetriminoArray);
+	copyArray2D(tmp, tetriminoArray[tetriminoShape]);
 }
 
 // ------------------------------------------------------------
@@ -185,5 +160,24 @@ void copyArray2D(int sourceArray[][NUM_COLS], int destArray[][NUM_COLS]) {
 		for (int col = 0; col < NUM_COLS; col++) {
 			destArray[row][col] = sourceArray[row][col];
 		}
+	}
+}
+
+char tetriminoName(int tetrimino) {
+	switch (tetrimino) {
+	case 0:
+		return 'I';
+	case 1:
+		return 'J';
+	case 2:
+		return 'L';
+	case 3:
+		return 'O';
+	case 4:
+		return 'S';
+	case 5:
+		return 'T';
+	case 6:
+		return 'Z';
 	}
 }
