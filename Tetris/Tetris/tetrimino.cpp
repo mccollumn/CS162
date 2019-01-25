9d07326dd5642b771df3a1d11fecf91a5df82058
Nick McCollum <nmccollum@frontier.com>
2019-01-25 08:31:30 -0800

    Tetrimino class

---
+++ Tetris/Tetris/tetrimino.cpp
@@ -0,0 +1,205 @@
+/*
+@Filename:		tetrimino.cpp
+@Author:		Nick McCollum
+@Date:			1/25/2019
+@Version:		1.0
+@Dev env:		VS 2017
+@Description:	Class that generates a tetrimino object with methods for manipulating the tetrimino.
+*/
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include "tetrimino.h"
+
+using namespace std;
+
+const int NUM_TETRIMINOS = 7;
+
+Tetrimino::Tetrimino(int type) {
+	location.row = 0;
+	location.col = 0;
+
+	if (type < 0 || type > 6) {
+		type = randType();
+	}
+
+	initGrid(type);
+
+	char colors[NUM_TETRIMINOS] = { 't', 'b', 'o', 'y', 'g', 'p', 'r' };
+	color = colors[type];
+}
+
+int Tetrimino::randType() {
+	static bool firstTime = true;
+	if (firstTime) {
+		srand(static_cast<unsigned int>(time(NULL)));
+		firstTime = false;
+	}
+	return rand() % 7;
+}
+
+void Tetrimino::initGrid(const int type) {
+
+	for (int row = 0; row < TETRIMINO_GRID_SIZE; row++) {
+		for (int col = 0; col < TETRIMINO_GRID_SIZE; col++) {
+			grid[row][col] = 0;
+		}
+	}
+
+	int shapeCoords[NUM_TETRIMINOS][TETRIMINO_GRID_SIZE][TETRIMINO_GRID_SIZE] = {
+	{{1,0},{1,1},{1,2},{1,3}}, // I
+	{{1,1},{2,1},{2,2},{2,3}}, // J
+	{{1,3},{2,1},{2,2},{2,3}}, // L
+	{{1,1},{1,2},{2,1},{2,2}}, // O
+	{{1,2},{1,3},{2,1},{2,2}}, // S
+	{{1,2},{2,1},{2,2},{2,3}}, // T
+	{{1,1},{1,2},{2,2},{2,3}}  // Z
+	};
+	for (int coord = 0; coord < TETRIMINO_GRID_SIZE; coord++) {
+		grid[shapeCoords[type][coord][0]][shapeCoords[type][coord][1]] = 1;
+	}
+}
+
+void Tetrimino::printArray(int array[][TETRIMINO_GRID_SIZE]) {
+	for (int row = 0; row < TETRIMINO_GRID_SIZE; row++) {
+		for (int col = 0; col < TETRIMINO_GRID_SIZE; col++) {
+			cout << array[row][col];
+		}
+		cout << endl;
+	}
+}
+
+void Tetrimino::copyArray2D(const int sourceArray[][TETRIMINO_GRID_SIZE]) {
+	for (int row = 0; row < TETRIMINO_GRID_SIZE; row++) {
+		for (int col = 0; col < TETRIMINO_GRID_SIZE; col++) {
+			grid[row][col] = sourceArray[row][col];
+		}
+	}
+}
+
+char Tetrimino::getColor() {
+	return color;
+}
+
+Location Tetrimino::getLocation() {
+	return location;
+}
+
+void Tetrimino::getGrid(int gridOut[][TETRIMINO_GRID_SIZE]) {
+	for (int row = 0; row < TETRIMINO_GRID_SIZE; row++) {
+		for (int col = 0; col < TETRIMINO_GRID_SIZE; col++) {
+			gridOut[row][col] = grid[row][col];
+		}
+	}
+}
+
+void Tetrimino::setLocation(Location newLocation) {
+	location = newLocation;
+}
+
+void Tetrimino::setLocation(int row, int col) {
+	location.row = row;
+	location.col = col;
+}
+
+void Tetrimino::rotateLeft() {
+	int tmp[TETRIMINO_GRID_SIZE][TETRIMINO_GRID_SIZE];
+	for (int row = 0; row < TETRIMINO_GRID_SIZE; row++) {
+		for (int col = 0; col < TETRIMINO_GRID_SIZE; col++) {
+			tmp[(TETRIMINO_GRID_SIZE - 1) - col][row] = grid[row][col];
+		}
+	}
+	copyArray2D(tmp);
+}
+
+void Tetrimino::rotateRight() {
+	int tmp[TETRIMINO_GRID_SIZE][TETRIMINO_GRID_SIZE];
+	for (int row = 0; row < TETRIMINO_GRID_SIZE; row++) {
+		for (int col = 0; col < TETRIMINO_GRID_SIZE; col++) {
+			tmp[col][(TETRIMINO_GRID_SIZE - 1) - row] = grid[row][col];
+		}
+	}
+	copyArray2D(tmp);
+}
+
+void Tetrimino::moveLeft() {
+	location.col--;
+}
+
+void Tetrimino::moveRight() {
+	location.col++;
+}
+
+void Tetrimino::moveDown() {
+	location.row++;
+}
+
+void Tetrimino::moveUp() {
+	location.row--;
+}
+
+void Tetrimino::dataDump() {
+	cout << "Current location: " << location.row << "," << location.col << endl;
+	cout << "Color: " << color << endl;
+	printArray(grid);
+	cout << endl;
+}
+
+int main() {
+
+	int tetriminoType = 2;
+
+	Tetrimino randomTetrimino;
+	Tetrimino myTetrimino(tetriminoType);
+
+	cout << "Type " << tetriminoType << " Tetrimino Data Dump\n";
+	myTetrimino.dataDump();
+
+	cout << "Random Tetrimino Data Dump\n";
+	randomTetrimino.dataDump();
+
+	cout << "Get Color: " << randomTetrimino.getColor() << endl << endl;
+
+	cout << "Get Location: " << randomTetrimino.getLocation().row << "," << randomTetrimino.getLocation().col << endl << endl;
+
+	int gridOutput[TETRIMINO_GRID_SIZE][TETRIMINO_GRID_SIZE];
+	randomTetrimino.getGrid(gridOutput);
+	cout << "Get Grid: \n";
+	randomTetrimino.printArray(gridOutput);
+	cout << endl;
+
+	randomTetrimino.setLocation({ 2, 3 });
+	cout << "Set Location (using location variable) Data Dump\n";
+	randomTetrimino.dataDump();
+
+	randomTetrimino.setLocation(4, 1);
+	cout << "Set Location (using separate int variables) Data Dump\n";
+	randomTetrimino.dataDump();
+
+	randomTetrimino.rotateLeft();
+	cout << "Rotate Left Data Dump\n";
+	randomTetrimino.dataDump();
+
+	randomTetrimino.rotateRight();
+	cout << "Rotate Right Data Dump\n";
+	randomTetrimino.dataDump();
+
+	randomTetrimino.moveLeft();
+	cout << "Move Left Data Dump\n";
+	randomTetrimino.dataDump();
+
+	randomTetrimino.moveRight();
+	cout << "Move Right Data Dump\n";
+	randomTetrimino.dataDump();
+
+	randomTetrimino.moveDown();
+	cout << "Move Down Data Dump\n";
+	randomTetrimino.dataDump();
+
+	randomTetrimino.moveUp();
+	cout << "Move Up Data Dump\n";
+	randomTetrimino.dataDump();
+
+	return 1;
+}
---
+++ Tetris/Tetris/tetrimino.h
@@ -0,0 +1,82 @@
+/*
+@Filename:		tetrimino.h
+@Author:		Nick McCollum
+@Date:			1/25/2019
+@Version:		1.0
+@Dev env:		VS 2017
+@Description:	Interface to tetrimino class.
+*/
+
+#ifndef TETRIS_TETRIMINO
+#define TETRIS_TETRIMINO
+
+const int TETRIMINO_GRID_SIZE = 4;
+
+struct Location {
+	int row;
+	int col;
+};
+
+class Tetrimino {
+private:
+	int grid[TETRIMINO_GRID_SIZE][TETRIMINO_GRID_SIZE]; //contains only zeros and ones 
+	char color;
+	Location location;
+
+	/*
+	initGrid: Sets the initial array values (shape) for each tetrimino.
+	@param: int Shape of tetrimino
+	@return: none
+	*/
+	void initGrid(const int type);
+
+	/*
+	randType: Generates a random tetrimino type (0 - 6)
+	@param: none
+	@return: int tetrimino type
+	*/
+	int randType();
+
+	/*
+	copyArray2D: Copies the contents of a 2D int array with known size to another array of the same size
+	@params: int Source Array
+	@return: none
+	*/
+	void copyArray2D(const int sourceArray[][TETRIMINO_GRID_SIZE]);
+
+public:
+	// constructor
+	// Initialize grid, color, and location
+	// The ‘type’ parameter will determine which piece we initialize grid to
+	Tetrimino(int type = 7); // valid type values are 0-6
+
+	//---------------------------------------------
+	//accessors
+	char getColor(); // returns the color of the tetrimino object
+	Location getLocation(); // return the location of the Tetrimino
+	void getGrid(int gridOut[][TETRIMINO_GRID_SIZE]);  // return the grid of the Tetrimino 
+
+	//---------------------------------------------
+	//mutators
+	void setLocation(Location newLocation); // modify location.row and location.col
+	void setLocation(int row, int col); // modify location.row and location.col
+
+	void rotateLeft();
+	void rotateRight();
+	void moveLeft();
+	void moveRight();
+	void moveDown();
+	void moveUp();
+
+	//---------------------------------------------
+	//others
+	void dataDump(); // print out the value of grid, color, and location
+
+	/*
+	printArray: prints the tetrimino array to std output
+	@params: int Array
+	@return: none
+	*/
+	void printArray(int array[][TETRIMINO_GRID_SIZE]);
+};
+#endif
