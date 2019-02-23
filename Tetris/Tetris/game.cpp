a94713010b247f744324ff969d26111ed5e03dd5
Nick McCollum <nmccollum@frontier.com>
2019-02-23 08:19:59 -0800

    Tetris game class complete

--- Tetris/Tetris/game.cpp
+++ Tetris/Tetris/game.cpp
@@ -10,6 +10,38 @@
 #include "game.h"
 
 Game::Game() {
+	gameWell = nullptr;
+	tetriminoInPlay = nullptr;
+	tetriminoNext = nullptr;
+	score = 0;
+	level = 1;
+	currentDropRate = TETRIMINO_DROP_RATE;
+	currentMusicPitch = 1.0;
+
+	// Font
+	if (!font.loadFromFile("ZCOOLQingKeHuangYou-Regular.ttf")) {
+		printError("ZCOOLQingKeHuangYou-Regular.ttf");
+	}
+
+	// Background image
+	if (!backgroundImage.loadFromFile("background.jpg")) {
+		printError("background.jpg");
+	}
+	background.setTexture(backgroundImage);
+	background.setColor(sf::Color(255, 255, 255, 128));
+
+	// Music
+	if (!music.openFromFile("tetris.ogg")) {
+		printError("tetris.oog");
+	}
+	music.setVolume(10.f);
+	music.setLoop(true);
+
+	// Tetrimino block
+	tetriminoBlock.setSize(sf::Vector2f(BLOCK_SIZE_PIXELS, BLOCK_SIZE_PIXELS));
+	tetriminoBlock.setOutlineColor(sf::Color(50, 50, 50));
+
+	// Game window
 	window.create(
 		sf::VideoMode(LAYOUT_WINDOW_WIDTH, LAYOUT_WINDOW_HEIGHT),
 		"Tetris 2019",
@@ -41,9 +73,269 @@ void Game::printError(std::string filename) {
 	std::cout << "Error: Cannot load " << filename << std::endl;
 }
 
-void Game::drawWell(Well gameWell, int top, int left, int blockWidth, sf::RectangleShape& block, sf::RectangleShape& well) {
+void Game::updateLevel() {
+	int increase = score / ROWS_PER_LEVEL;
+	if (increase >= level) {
+		level += increase;
+		for (int x = 0; x < increase; x++) {
+			currentDropRate -= currentDropRate * DROP_RATE_PER_LEVEL;
+			currentMusicPitch += currentMusicPitch * MUSIC_RATE_PER_LEVEL;
+		}
+		music.setPitch(currentMusicPitch);
+	}
+}
+
+void Game::processSplash() {
+
+	// Title
+	sf::Text title;
+	title.setFont(font);
+	title.setString("Tetris");
+	title.setCharacterSize(150);
+	title.setFillColor(sf::Color::Black);
+	sf::FloatRect titleRect = title.getLocalBounds();
+	title.setOrigin(titleRect.left + titleRect.width / 2.0f, titleRect.top + titleRect.height / 2.0f);
+	title.setPosition(sf::Vector2f(LAYOUT_WINDOW_WIDTH / 2.0f, LAYOUT_WINDOW_HEIGHT / 6.0f));
+	
+	// Instructions
+	std::string instructionText = "Controls:\nLeft Arrow - Move Piece Left\nRight Arrow - Move Piece Right\nUp Arrow - Rotate Piece\nDown Arrow - Drop Piece\n\nESC - Quite Game\nM - Toggle Music\nP - Pause Game";
+	sf::Text instructions;
+	instructions.setFont(font);
+	instructions.setString(instructionText);
+	instructions.setCharacterSize(25);
+	instructions.setFillColor(sf::Color::Black);
+	sf::FloatRect instructionsRect = instructions.getLocalBounds();
+	instructions.setOrigin(instructionsRect.left + instructionsRect.width / 2.0f, instructionsRect.top + instructionsRect.height / 2.0f);
+	instructions.setPosition(sf::Vector2f(LAYOUT_WINDOW_WIDTH / 2.0f, LAYOUT_WINDOW_HEIGHT / 1.8f));
+	
+	// Start message
+	sf::Text start;
+	start.setFont(font);
+	start.setString("Press any key to Start");
+	start.setCharacterSize(50);
+	start.setFillColor(sf::Color::Black);
+	sf::FloatRect startRect = start.getLocalBounds();
+	start.setOrigin(startRect.left + startRect.width / 2.0f, startRect.top + startRect.height / 2.0f);
+	start.setPosition(sf::Vector2f(LAYOUT_WINDOW_WIDTH / 2.0f, LAYOUT_WINDOW_HEIGHT / 1.1f));
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::KeyPressed) {
+				return;
+			}
+		}
+		window.clear(sf::Color::White);
+		window.draw(background);
+		window.draw(title);
+		window.draw(instructions);
+		window.draw(start);
+		window.display();
+	}
+}
+
+void Game::processGame() {
+
+	// Click sound
+	sf::SoundBuffer bufferClick;
+	if (!bufferClick.loadFromFile("click.wav")) {
+		printError("click.wav");
+	}
+	sf::Sound soundClick;
+	soundClick.setBuffer(bufferClick);
+
+	// Click sound
+	sf::SoundBuffer bufferClear;
+	if (!bufferClear.loadFromFile("clear.wav")) {
+		printError("clear.wav");
+	}
+	sf::Sound soundClear;
+	soundClear.setBuffer(bufferClear);
+
+	// Error sound
+	sf::SoundBuffer bufferError;
+	if (!bufferError.loadFromFile("error.wav")) {
+		printError("error.wav");
+	}
+	sf::Sound soundError;
+	soundError.setBuffer(bufferError);
+	soundError.setVolume(50.f);
+
+	// Well block
+	sf::RectangleShape wellBlock;
+	wellBlock.setSize(sf::Vector2f(BLOCK_SIZE_PIXELS, BLOCK_SIZE_PIXELS));
+	wellBlock.setOutlineThickness(-1);
+
+	// Well graphic
+	sf::RectangleShape well;
+	well.setSize(sf::Vector2f(WELL_WIDTH * BLOCK_SIZE_PIXELS, WELL_HEIGHT * BLOCK_SIZE_PIXELS));
+	well.setOutlineColor(sf::Color::Black);
+	well.setOutlineThickness(1);
+	well.setFillColor(sf::Color::Transparent);
+
+	// Tetriminos
+	tetriminoInPlay = new Tetrimino;
+	tetriminoInPlay->setLocation(TETRIMINO_START_LOCATION);
+	tetriminoNext = new Tetrimino;
+
+	//Game Well
+	gameWell = new Well;
+
+	int moveCounter = 0;
+	bool paused = false;
+
+	music.play();
+
+	while (window.isOpen() && !gameWell->topReached()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+				return;
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left) {
+				tetriminoInPlay->moveLeft();
+				if (!gameWell->tetriminoFit(*tetriminoInPlay)) {
+					tetriminoInPlay->moveRight();
+					soundError.play();
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right) {
+				tetriminoInPlay->moveRight();
+				if (!gameWell->tetriminoFit(*tetriminoInPlay)) {
+					tetriminoInPlay->moveLeft();
+					soundError.play();
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up) {
+				tetriminoInPlay->rotateRight();
+				if (!gameWell->tetriminoFit(*tetriminoInPlay)) {
+					tetriminoInPlay->rotateLeft();
+					soundError.play();
+				}
+				soundClick.play();
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down) {
+				tetriminoInPlay->moveDown();
+				if (!gameWell->tetriminoFit(*tetriminoInPlay)) {
+					tetriminoInPlay->moveUp();
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::M) {
+				int status = music.getStatus();
+				if (status == 2) {
+					music.stop();
+				}
+				if (status == 0 || status == 1) {
+					music.play();
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P) {
+				if (!paused) {
+					paused = true;
+				}
+				else if (paused) {
+					paused = false;
+				}
+			}
+		}
+
+		moveCounter++;
+		if (moveCounter >= currentDropRate && !paused) {
+			tetriminoInPlay->moveDown();
+			if (!gameWell->tetriminoFit(*tetriminoInPlay)) {
+				tetriminoInPlay->moveUp();
+				gameWell->addTetriminoToWell(*tetriminoInPlay);
+				delete tetriminoInPlay;
+				int rowsCleared = gameWell->clearFullRows();
+				if (rowsCleared == 4) {
+					soundClear.play();
+					score += rowsCleared * TETRIS_MULTIPLIER;
+					updateLevel();
+				}
+				else if (rowsCleared > 0) {
+					soundClear.play();
+					score += rowsCleared;
+					updateLevel();
+				}
+				if (!gameWell->topReached()) {
+					tetriminoInPlay = tetriminoNext;
+					tetriminoInPlay->setLocation(TETRIMINO_START_LOCATION);
+					tetriminoNext = new Tetrimino;
+				}
+			}
+			moveCounter = 0;
+		}
+
+		window.clear(sf::Color::White);
+		window.draw(background);
+		drawNext(tetriminoNext, LAYOUT_NEXT_TOP, LAYOUT_NEXT_LEFT);
+		drawWell(gameWell, LAYOUT_BOARD_TOP, LAYOUT_BOARD_LEFT, BLOCK_SIZE_PIXELS, wellBlock, well);
+		drawTetrimino(tetriminoInPlay, LAYOUT_BOARD_TOP, LAYOUT_BOARD_LEFT, BLOCK_SIZE_PIXELS, tetriminoBlock);
+		drawScore(score, LAYOUT_SCORE_TOP, LAYOUT_SCORE_LEFT);
+		window.display();
+	}
+}
+
+bool Game::processGameOver() {
+
+	// Thanks
+	sf::Text thanks;
+	thanks.setFont(font);
+	thanks.setString("Thanks for Playing");
+	thanks.setCharacterSize(50);
+	thanks.setFillColor(sf::Color::Black);
+	sf::FloatRect thanksRect = thanks.getLocalBounds();
+	thanks.setOrigin(thanksRect.left + thanksRect.width / 2.0f, thanksRect.top + thanksRect.height / 2.0f);
+	thanks.setPosition(sf::Vector2f(LAYOUT_WINDOW_WIDTH / 2.0f, LAYOUT_WINDOW_HEIGHT / 6.0f));
+
+	// Try Again
+	sf::Text playAgain;
+	playAgain.setFont(font);
+	playAgain.setString("Press Any Key to Try Again\n");
+	playAgain.setCharacterSize(25);
+	playAgain.setFillColor(sf::Color::Black);
+	sf::FloatRect playAgainRect = playAgain.getLocalBounds();
+	playAgain.setOrigin(playAgainRect.left + playAgainRect.width / 2.0f, playAgainRect.top + playAgainRect.height / 2.0f);
+	playAgain.setPosition(sf::Vector2f(LAYOUT_WINDOW_WIDTH / 2.0f, LAYOUT_WINDOW_HEIGHT / 1.25f));
+
+	// Quit
+	sf::Text quit;
+	quit.setFont(font);
+	quit.setString("Press ESC to Quit");
+	quit.setCharacterSize(25);
+	quit.setFillColor(sf::Color::Black);
+	sf::FloatRect quitRect = quit.getLocalBounds();
+	quit.setOrigin(quitRect.left + quitRect.width / 2.0f, quitRect.top + quitRect.height / 2.0f);
+	quit.setPosition(sf::Vector2f(LAYOUT_WINDOW_WIDTH / 2.0f, LAYOUT_WINDOW_HEIGHT / 1.20f));
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
+				window.close();
+			if (event.type == sf::Event::KeyPressed) {
+				return true;
+			}
+		}
+		window.clear(sf::Color::White);
+		window.draw(background);
+		window.draw(thanks);
+		drawScore(score, 150, 150);
+		window.draw(playAgain);
+		window.draw(quit);
+		window.display();
+	}
+	return false;
+}
+
+void Game::drawWell(Well* gameWell, int top, int left, int blockWidth, sf::RectangleShape& block, sf::RectangleShape& well) {
 	char gameBoard[WELL_HEIGHT][WELL_WIDTH];
-	gameWell.getBoard(gameBoard);
+	gameWell->getBoard(gameBoard);
 	int currentX = left;
 	int currentY = top;
 
@@ -70,20 +362,20 @@ void Game::drawWell(Well gameWell, int top, int left, int blockWidth, sf::Rectan
 	window.draw(well);
 }
 
-void Game::drawTetrimino(Tetrimino tetrimino, int top, int left, int blockWidth, sf::RectangleShape& block) {
+void Game::drawTetrimino(Tetrimino* tetrimino, int top, int left, int blockWidth, sf::RectangleShape& block) {
 	int grid[TETRIMINO_GRID_SIZE][TETRIMINO_GRID_SIZE];
-	tetrimino.getGrid(grid);
-	Location currentLocation = tetrimino.getLocation();
+	tetrimino->getGrid(grid);
+	Location currentLocation = tetrimino->getLocation();
 	int currentX = left + (currentLocation.col * blockWidth);
 	int currentY = top + (currentLocation.row * blockWidth);
-	char color = tetrimino.getColor();
+	char color = tetrimino->getColor();
 
 	for (int row = 0; row < TETRIMINO_GRID_SIZE; row++) {
 		for (int col = 0; col < TETRIMINO_GRID_SIZE; col++) {
 			block.setPosition(currentX, currentY);
 			if (grid[row][col] == 1 && currentY >= 0) {
 				block.setFillColor(convertToSfmlColor(color));
-				block.setOutlineThickness(1);
+				block.setOutlineThickness(-1);
 			}
 			else {
 				block.setFillColor(sf::Color::Transparent);
@@ -97,102 +389,95 @@ void Game::drawTetrimino(Tetrimino tetrimino, int top, int left, int blockWidth,
 	}
 }
 
+void Game::drawScore(int score, int top, int left) {
+
+	// Score label
+	sf::Text text;
+	text.setFont(font);
+	text.setString("Score");
+	text.setCharacterSize(20);
+	text.setFillColor(sf::Color::Black);
+	text.setPosition(left, top);
+
+	// Score
+	sf::Text scoreText;
+	scoreText.setFont(font);
+	scoreText.setString(std::to_string(score));
+	scoreText.setCharacterSize(20);
+	scoreText.setFillColor(sf::Color::Black);
+	sf::FloatRect scoreTextRect = scoreText.getLocalBounds();
+	scoreText.setOrigin(scoreTextRect.left + scoreTextRect.width / 2.0f, scoreTextRect.top + scoreTextRect.height / 2.0f);
+	scoreText.setPosition(sf::Vector2f(left + LAYOUT_SCORE_WIDTH / 2.0f, top + 25 + LAYOUT_SCORE_HEIGHT / 2.0f));
+
+	// Score border
+	sf::RectangleShape border(sf::Vector2f(LAYOUT_SCORE_WIDTH, LAYOUT_SCORE_HEIGHT));
+	border.setOutlineColor(sf::Color::Black);
+	border.setOutlineThickness(1);
+	border.setFillColor(sf::Color::White);
+	border.setPosition(left, top + 25);
+
+	window.draw(border);
+	window.draw(text);
+	window.draw(scoreText);
+	text.setString("Level");
+	text.setPosition(left, top + 100);
+	border.setPosition(left, top + 125);
+	scoreText.setString(std::to_string(level));
+	scoreText.setPosition(sf::Vector2f(left + LAYOUT_SCORE_WIDTH / 2.0f, top + 125 + LAYOUT_SCORE_HEIGHT / 2.0f));
+	window.draw(text);
+	window.draw(border);
+	window.draw(scoreText);
+}
+
+void Game::drawNext(Tetrimino* tetrimino, int top, int left) {
+
+	// Next piece label
+	sf::Text text;
+	text.setFont(font);
+	text.setString("Next Piece");
+	text.setCharacterSize(20);
+	text.setFillColor(sf::Color::Black);
+	text.setPosition(left, top);
+
+	// Border
+	sf::RectangleShape border(sf::Vector2f(LAYOUT_NEXT_WIDTH, LAYOUT_NEXT_HEIGHT));
+	border.setOutlineColor(sf::Color::Black);
+	border.setOutlineThickness(1);
+	border.setFillColor(sf::Color::White);
+	border.setPosition(left, top + 25);
+
+	window.draw(text);
+	window.draw(border);
+	//TODO: Center the piece. Different pieces sit slightly differntly in the grid.
+	drawTetrimino(tetriminoNext, top + 35, left + 5, BLOCK_SIZE_PIXELS, tetriminoBlock);
+}
+
 void Game::playGame() {
-
-	// Click sound
-	sf::SoundBuffer bufferClick;
-	if (!bufferClick.loadFromFile("click.wav")) {
-		printError("click.wav");
-	}
-	sf::Sound soundClick;
-	soundClick.setBuffer(bufferClick);
-
-	// Error sound
-	sf::SoundBuffer bufferError;
-	if (!bufferError.loadFromFile("error.wav")) {
-		printError("error.wav");
-	}
-	sf::Sound soundError;
-	soundError.setBuffer(bufferError);
-	soundError.setVolume(50.f);
-
-	// Music
-	sf::Music music;
-	if (!music.openFromFile("tetris.ogg")) {
-		printError("tetris.oog");
-	}
-	music.setVolume(10.f);
-	music.setLoop(true);
-	music.play();
-
-	// Background image
-	sf::Texture texture;
-	if (!texture.loadFromFile("background.jpg")) {
-		printError("background.jpg");
-	}
-	sf::Sprite background;
-	background.setTexture(texture);
-
-	// Tetrimino block
-	sf::RectangleShape tetriminoBlock;
-	tetriminoBlock.setSize(sf::Vector2f(BLOCK_SIZE_PIXELS, BLOCK_SIZE_PIXELS));
-	tetriminoBlock.setOutlineColor(sf::Color(50, 50, 50));
-
-	// Well block
-	sf::RectangleShape wellBlock;
-	wellBlock.setSize(sf::Vector2f(BLOCK_SIZE_PIXELS, BLOCK_SIZE_PIXELS));
-	wellBlock.setOutlineThickness(1);
-
-	// Well
-	sf::RectangleShape well;
-	well.setSize(sf::Vector2f(WELL_WIDTH * BLOCK_SIZE_PIXELS, WELL_HEIGHT * BLOCK_SIZE_PIXELS));
-	well.setOutlineColor(sf::Color::Black);
-	well.setOutlineThickness(1);
-	well.setFillColor(sf::Color::Transparent);
+	GameState currentGameState = TETRIS_SPLASH;
 
 	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
+		switch (currentGameState) {
+		case TETRIS_SPLASH:
+			processSplash();
+			currentGameState = TETRIS_PLAY;
+			break;
+		case TETRIS_PLAY:
+			processGame();
+			currentGameState = TETRIS_GAME_OVER;
+			break;
+		case TETRIS_GAME_OVER:
+			if (processGameOver()) {
+				score = 0;
+				level = 1;
+				currentDropRate = TETRIMINO_DROP_RATE;
+				music.setPitch(1.0f);
+				currentGameState = TETRIS_PLAY;
+			}
+			else {
 				window.close();
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left) {
-				tetriminoInPlay.moveLeft();
-				if (!gameWell.tetriminoFit(tetriminoInPlay)) {
-					tetriminoInPlay.moveRight();
-					soundError.play();
-				}
 			}
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right) {
-				tetriminoInPlay.moveRight();
-				if (!gameWell.tetriminoFit(tetriminoInPlay)) {
-					tetriminoInPlay.moveLeft();
-					soundError.play();
-				}
-			}
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up) {
-				tetriminoInPlay.rotateRight();
-				if (!gameWell.tetriminoFit(tetriminoInPlay)) {
-					tetriminoInPlay.rotateLeft();
-					soundError.play();
-				}
-				soundClick.play();
-			}
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::M) {
-				int status = music.getStatus();
-				if (status == 2) {
-					music.stop();
-				}
-				if (status == 0 || status == 1) {
-					music.play();
-				}
-			}
+			break;
 		}
-
-		window.clear(sf::Color::White);
-		window.draw(background);
-		drawWell(gameWell, LAYOUT_BOARD_TOP, LAYOUT_BOARD_LEFT, BLOCK_SIZE_PIXELS, wellBlock, well);
-		drawTetrimino(tetriminoInPlay, LAYOUT_BOARD_TOP, LAYOUT_BOARD_LEFT, BLOCK_SIZE_PIXELS, tetriminoBlock);
-		window.display();
 	}
 }
 
--- Tetris/Tetris/game.h
+++ Tetris/Tetris/game.h
@@ -20,14 +20,40 @@ const int LAYOUT_WINDOW_WIDTH = 400;
 const int LAYOUT_WINDOW_HEIGHT = 500;
 const int LAYOUT_BOARD_TOP = 10;
 const int LAYOUT_BOARD_LEFT = 200;
+const int LAYOUT_NEXT_TOP = 10;
+const int LAYOUT_NEXT_LEFT = 50;
+const int LAYOUT_NEXT_WIDTH = 100;
+const int LAYOUT_NEXT_HEIGHT = 100;
+const int LAYOUT_SCORE_TOP = 200;
+const int LAYOUT_SCORE_LEFT = 50;
+const int LAYOUT_SCORE_WIDTH = 100;
+const int LAYOUT_SCORE_HEIGHT = 50;
 const int BLOCK_SIZE_PIXELS = 20;
+const int TETRIS_MULTIPLIER = 2;
+const Location TETRIMINO_START_LOCATION = { -2, 2 };
+const int TETRIMINO_DROP_RATE = 240;
+const int ROWS_PER_LEVEL = 1;				// Set low for demo purposes. Should probably be more like 10.
+const float DROP_RATE_PER_LEVEL = 0.5;		// Set high for demo purposes. You probably don't actually want it 50% faster each level.
+const float MUSIC_RATE_PER_LEVEL = 0.1;
+
+enum GameState { TETRIS_SPLASH, TETRIS_PLAY, TETRIS_GAME_OVER };
 
 class Game
 {
 private:
 	sf::RenderWindow window;
-	Well gameWell;
-	Tetrimino tetriminoInPlay;
+	sf::Font font;
+	sf::Texture backgroundImage;
+	sf::Sprite background;
+	sf::Music music;
+	sf::RectangleShape tetriminoBlock;
+	int score;
+	int level;
+	float currentDropRate;
+	float currentMusicPitch;
+	Well* gameWell;
+	Tetrimino* tetriminoInPlay;
+	Tetrimino* tetriminoNext;
 
 	/*
 	convertToSfmlColor: Converts a character representing the Tetrimino color into a SFML color value
@@ -44,18 +70,60 @@ private:
 	void printError(std::string filename);
 
 	/*
-	drawWell: Draws a well object in the SFML window
-	@param: Well gameWell, int pixels from top, int pixels from left, int pixel width of block
+	updateLevel: Checks the score and increases the game level if necessary
+	@param: none
 	@return: none
 	*/
-	void drawWell(Well gameWell, int top, int left, int blockWidth, sf::RectangleShape& block, sf::RectangleShape& well);
+	void updateLevel();
 
 	/*
-	drawTetrimino: Draws a tetrimino objec
-	@param: Tetrimino, int pixels from top, int pixels from left, int pixel width of block
+	processSplash: Displays a splash screen
+	@param: none
 	@return: none
 	*/
-	void drawTetrimino(Tetrimino tetrimino, int top, int left, int blockWidth, sf::RectangleShape& block);
+	void processSplash();
+
+	/*
+	processGame: Polls for game events and draws game well and tetrimino
+	@param: none
+	@return: none
+	*/
+	void processGame();
+
+	/*
+	processGameOver: Displays a game over screen
+	@param: none
+	@return: bool Play Again
+	*/
+	bool processGameOver();
+
+	/*
+	drawWell: Draws a well object in the SFML window
+	@param: Well Pointer gameWell, int pixels from top, int pixels from left, int pixel width of block
+	@return: none
+	*/
+	void drawWell(Well* gameWell, int top, int left, int blockWidth, sf::RectangleShape& block, sf::RectangleShape& well);
+
+	/*
+	drawTetrimino: Draws a tetrimino object
+	@param: Tetrimino Pointer, int pixels from top, int pixels from left, int pixel width of block
+	@return: none
+	*/
+	void drawTetrimino(Tetrimino* tetrimino, int top, int left, int blockWidth, sf::RectangleShape& block);
+
+	/*
+	drawScore: Displays the score
+	@param: int Score, int Position Top, int Position Left
+	@return: none
+	*/
+	void drawScore(int score, int top, int left);
+	
+	/*
+	drawNext: Display for the next tetrimino
+	@param: Tetrinimo Pointer, int pixels from top, int pixels from left
+	@return: none
+	*/
+	void drawNext(Tetrimino* tetrimino, int top, int left);
 
 public:
 	Game();
